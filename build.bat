ae74b4a853135d56c85ae7485c73aa26c349dbc7
martijnj <martijnj@S0000657>
2012-06-25 20:50:15 +1000

    added auto creation of the msvc binary zip to the build script

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@
 @set MSGPACKDIR=../msgpack-0.5.4
 cl /nologo /DMSGPACK_BUILDDLL /LD /Ox /O2 /W4 msgpackalt.c
 @del *.obj
+7z -mx9 u msgpackalt-msvc9.zip msgpackalt.c msgpackalt.dll msgpackalt.h msgpackalt.hpp msgpackalt.lib stdint_msc.h >NUL
