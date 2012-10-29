34b333578e9a18b2d08754477b482050cad674c1
martijnj <martijnj@flotilla.staff.monash.edu.au>
2012-10-29 13:24:04 +1100

    Corrected build script if original msgpack not installed Corrected i64-printf statement in test.cpp Made output of test.cpp and test.c more similar

--- tests/build.bat
+++ tests/build.bat
@@ -3,6 +3,8 @@
 cl /nologo /Ox /O2 /I.. test.c
 cl /nologo /Ox /O2 /I.. /EHsc /Tp test.cpp /Fetestcpp
 cl /nologo /Ox /O2 /I.. speed_test.c
+@if not exist %MSGPACKDIR% goto done
 cl /nologo /Ox /O2 /MD /I../%MSGPACKDIR%/src /Tp speed_test0.c /link ws2_32.lib ../%MSGPACKDIR%/lib/msgpack.lib
 cl /nologo /Ox /O2 /MD /I../%MSGPACKDIR%/src /Tp test0.c /link ws2_32.lib ../%MSGPACKDIR%/lib/msgpack.lib
-@del *.obj
+:done
+@del *.obj 2>NUL
--- tests/test.cpp
+++ tests/test.cpp
@@ -8,6 +8,7 @@ test.cpp : a demonstration of msgpackalt C++ classes
 */
 #include <algorithm>
 #include <cstdio>
+#include <iostream>
 
 #define MSGPACK_INLINE
 #include "msgpackalt.hpp"
@@ -18,18 +19,20 @@ void outhex( char c )   { std::printf( "%02X", ( byte )c ); }
 int main( )
 {
   uint32_t n;
-  const int8_t v0[] = {-1,-2,-3};
-  std::vector<int8_t> v(v0,v0+3); // static vector allocation
+  const int32_t v0[3] = {0,-1,-2};
+  std::vector<int64_t> v(v0,v0+3); // static vector allocation
   
   packer p;
   p << 12345u << "pi" << 3.14159 << -9876543210 << v;
   
   std::string s = p.string();
   std::for_each( s.begin(), s.end(), outhex );
+  printf( "\nPacked into %u bytes\n", s.size() );
   v.clear( ); std::string str; int64_t i64; double f;
   
   unpacker u(p);
   u >> n >> str >> f >> i64 >> v;
-  std::printf( "\nUnpacked with %u bytes remaining\n", u.len( ));
+  for ( int i = 0; i < v.size(); ++i ) printf( "%lli\n", v[i] );
+  std::printf( "Unpacked with %u bytes remaining\n", u.len( ));
   return 0;
 }
